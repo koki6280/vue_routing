981bd140a52548b3918781d0bdbc61bb3ae53d01
林幸輝 <K-h.too.brag.is.idiot@docomo.ne.jp>
2020-11-01 16:12:35 +0900

    vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,21 @@
   <div>
     <h3>Home</h3>
     <button @click="toUsers">Usersのページに行く</button>
+    <p>{{ doubleCount }}</p>
+    <p>{{ tripleCount }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    doubleCount() {
+      return this.$store.getters.doubleCount;
+    },
+    tripleCount() {
+      return this.$store.getters.tripleCount;
+    }
+  },
   methods: {
     toUsers() {
       this.$router.push({
